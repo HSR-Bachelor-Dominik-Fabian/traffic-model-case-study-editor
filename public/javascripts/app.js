2304ee037a94d3e222ffc4c549ae1dafacdc3ed6
fabiankeller <fabian.keller91@gmail.com>
2016-03-10 13:05:26 +0100

    extracted functionality of updateFeature into controller

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,41 @@
-require(["angular"], function(){
-    var simmapeditorApp = angular.module("simmapeditorApp",[]);
+require(["angular", "bootstrap", "leaflet", "angular-route"], function(){
+    var simmapeditorApp = angular.module("simmapeditorApp",['ngRoute']);
 
     simmapeditorApp.controller("StreetDetailController", function($scope){
-        $scope.streetModel=null
-        $scope.clicked=function(evt){
-            alert(evt);
+        $scope.streetModel = null
+        $scope.layer = null;
+        $scope.marker = null;
+        $scope.newFeature = function(feature, layer, latlng, map) {
+            var streetDetails = $("#streetDetails")
+            $scope.streetModel = feature;
+            $scope.layer = layer;
+            streetDetails.offcanvas("show");
+            layer.setStyle( {color: "yellow"} );
+            if ($scope.marker != null) {L.removeLayer(marker);};
+            $scope.marker = L.marker(latlng).addTo(map);
+            $scope.$apply();
+        };
+        $scope.removeFeature = function(map, beforeNew) {
+            var streetDetails = $("#streetDetails")
+            if (!beforeNew) streetDetails.offcanvas("hide");
+            $scope.streetModel = null;
+            var layer = $scope.layer;
+            if (layer != null) { layer.setStyle( {color: "red"} ); };
+            $scope.layer = null;
+            var marker = $scope.marker;
+            if (marker != null) {map.removeLayer(marker);};
+            $scope.marker = null;
+            $scope.$apply();
+        };
+        $scope.updateFeature = function(feature, layer, latlng, map) {
+            if ($scope.streetModel == null) {
+                $scope.newFeature(feature, layer, latlng, map);
+            } else if (feature == null) {
+                $scope.removeFeature(map, false);
+            } else {
+                $scope.removeFeature(map, true);
+                $scope.newFeature(feature, layer, latlng, map);
+            }
         }
     });
 });
--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -4,12 +4,14 @@ var require = {
         "jquery":["https://code.jquery.com/jquery-1.12.1.min", "jquery-1.9.0.min"],
         "d3": ["//d3js.org/d3.v3.min", "d3.v3.min"],
         "angular": ["https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min"],
+        "angular-route": ["https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-route"],
         "bootstrap":["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min"],
         "jasny": ["https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min"],
         "zoomslider": ["L.Control.Zoomslider"]
     },
     shim:{
         "bootstrap":{deps:["jquery"]},
-        "jasny":{deps:["bootstrap"]}
+        "jasny":{deps:["bootstrap"]},
+        "angular-route":{deps:["angular"]}
     }
 };
