9050cc19adbcd20198c4a9a95edd3410d2cd1dc2
fabiankeller <fabian.keller91@gmail.com>
2016-03-10 17:22:52 +0100

    removed requirejs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,41 +1,54 @@
-require(["angular", "bootstrap", "leaflet", "angular-route"], function(){
-    var simmapeditorApp = angular.module("simmapeditorApp",['ngRoute']);
+(function(){
+    var simmapeditorApp = angular.module("simmapeditorApp", []);
 
-    simmapeditorApp.controller("StreetDetailController", ["$scope", function($scope){
+    simmapeditorApp.controller("StreetDetailController", ['$scope', function($scope) {
         $scope.streetModel = null
         $scope.layer = null;
         $scope.marker = null;
-        $scope.newFeature = function(feature, layer, latlng, map) {
+        $scope.newFeature = function (feature, layer, latlng, map) {
             var streetDetails = $("#streetDetails")
             $scope.streetModel = feature;
             $scope.layer = layer;
             streetDetails.offcanvas("show");
-            layer.setStyle( {color: "yellow"} );
-            if ($scope.marker != null) {L.removeLayer(marker);};
+            layer.setStyle({color: "yellow"});
+            if ($scope.marker != null) {
+                L.removeLayer(marker);
+            }
+            ;
             $scope.marker = L.marker(latlng).addTo(map);
             $scope.$apply();
         };
-        $scope.removeFeature = function(map, beforeNew) {
+        $scope.removeFeature = function (map, beforeNew) {
             var streetDetails = $("#streetDetails")
             if (!beforeNew) streetDetails.offcanvas("hide");
             $scope.streetModel = null;
             var layer = $scope.layer;
-            if (layer != null) { layer.setStyle( {color: "red"} ); };
+            if (layer != null) {
+                layer.setStyle({color: "red"});
+            }
+            ;
             $scope.layer = null;
             var marker = $scope.marker;
-            if (marker != null) {map.removeLayer(marker);};
+            if (marker != null) {
+                map.removeLayer(marker);
+            }
+            ;
             $scope.marker = null;
             $scope.$apply();
         };
-        $scope.updateFeature = function(feature, layer, latlng, map) {
-            if ($scope.streetModel == null) {
-                $scope.newFeature(feature, layer, latlng, map);
-            } else if (feature == null) {
-                $scope.removeFeature(map, false);
+        $scope.updateFeature = function (feature, layer, latlng, map) {
+            if ($scope.streetModel != null) {
+                if (feature == null) {
+                    $scope.removeFeature(map, false)
+                } else {
+                    $scope.removeFeature(map, true);
+                    $scope.newFeature(feature, layer, latlng, map);
+                }
             } else {
-                $scope.removeFeature(map, true);
-                $scope.newFeature(feature, layer, latlng, map);
+                if (feature != null) {
+                    $scope.newFeature(feature, layer, latlng, map);
+                }
             }
         }
     }]);
-});
+})();
