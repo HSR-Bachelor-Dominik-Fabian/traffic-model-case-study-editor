30673b4ebc6857e61f607a8108c8c440a7fe6616
Dominik Heeb <do.heeb@hotmail.com>
2016-03-23 12:55:21 +0100

    added postgres docker

--- docker-compose/postgres/simmapdatabase.sql
+++ docker-compose/postgres/simmapdatabase.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.5.1
 -- Dumped by pg_dump version 9.5.1
 
--- Started on 2016-03-22 09:31:56
+-- Started on 2016-03-23 12:50:27
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -57,7 +57,7 @@ COMMENT ON SCHEMA public IS 'standard public schema';
 
 --
 -- TOC entry 1 (class 3079 OID 12355)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
@@ -66,7 +66,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 --
 -- TOC entry 2126 (class 0 OID 0)
 -- Dependencies: 1
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -262,7 +262,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2016-03-22 09:31:56
+-- Completed on 2016-03-23 12:50:27
 
 --
 -- PostgreSQL database dump complete
