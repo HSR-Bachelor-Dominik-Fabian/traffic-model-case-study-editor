685676064d85d6a5f2e1325470bb47a26add56ab
Dominik Heeb <do.heeb@hotmail.com>
2016-03-25 22:45:08 +0100

    Increasing Speed with Cache Test

--- Simmapservice/src/main/java/simmapservice/resources/QuadtileResource.java
+++ Simmapservice/src/main/java/simmapservice/resources/QuadtileResource.java
@@ -1,15 +1,18 @@
 package simmapservice.resources;
 
 import businesslogic.datafetch.DataFetchLogic;
+import com.google.common.base.Stopwatch;
 import org.json.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by fke on 17.03.2016.
@@ -28,8 +31,16 @@ public class QuadtileResource {
     public Response getQuadTile(@PathParam("z") int z, @PathParam("x") int x, @PathParam("y") int y, @PathParam("networkid") int networkID) {
 
         DataFetchLogic dataFetchLogic = new DataFetchLogic();
+        Stopwatch stopwatch = Stopwatch.createStarted();
         JSONObject json =  dataFetchLogic.getDataForTile(x, y, z, networkID, this.properties);
-
-        return Response.ok(json.toString()).build();
+        stopwatch.stop();
+        System.out.println("Get Data: " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(88400);
+        cc.setPrivate(true);
+        
+        Response.ResponseBuilder builder = Response.ok(json.toString());
+        builder.cacheControl(cc);
+        return builder.build();
     }
 }
