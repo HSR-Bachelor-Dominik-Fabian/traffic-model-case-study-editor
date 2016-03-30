be4710c23ede43bb2d4413fe1c0a97ff72926ccc
fabiankeller <fabian.keller91@gmail.com>
2016-03-30 16:39:16 +0200

    xml streaming parser for nodes and links

--- Simmapservice/src/main/java/simmapservice/resources/XMLImportResource.java
+++ Simmapservice/src/main/java/simmapservice/resources/XMLImportResource.java
@@ -1,9 +1,15 @@
 package simmapservice.resources;
 import businesslogic.xmlImport.XMLImportLogic;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.JSONObject;
 import org.json.XML;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -12,9 +18,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
 import java.util.Properties;
 
 /**
@@ -35,14 +42,17 @@ public class XMLImportResource {
                                @FormDataParam("format") String format, @Context HttpServletResponse response,
                                @FormDataParam("name") String networkName) throws IOException {
 
-        StringWriter writer = new StringWriter();
-        IOUtils.copy(inputStream, writer);
-        String theString = writer.toString();
-        JSONObject jsonObject = XML.toJSONObject(theString);
+        //String xml = IOUtils.toString(inputStream);
+        //JSONObject jsonObject = XML.toJSONObject(xml);
+
+        //StringWriter writer = new StringWriter();
+        //IOUtils.copy(inputStream, writer);
+        //String theString = writer.toString();
+        //JSONObject jsonObject = XML.toJSONObject(theString);
 
         XMLImportLogic importLogic = new XMLImportLogic(props);
-        importLogic.importNetwork2DB(jsonObject, format, networkName);
+        importLogic.importNetwork2DB(inputStream, format, networkName);
 
-        return Response.ok(theString).build();
+        return Response.ok().build();
     }
 }
