f11ed466be5dedfbe6050420883e1cbbe569d6c8
Dominik Heeb <do.heeb@hotmail.com>
2016-04-21 14:36:00 +0200

    Created Sidemenu in Angular to load Changeset

--- Simmapeditor/public/javascripts/app.js
+++ Simmapeditor/public/javascripts/app.js
@@ -1,12 +1,22 @@
 (function(){
     var simmapeditorApp = angular.module("simmapeditorApp", []);
+    simmapeditorApp.directive("rootmenu", function() {
+        return {templateUrl: "/partials/rootmenu"};
+    });
+    simmapeditorApp.directive("loadchangesetmenu", function() {
+        return {templateUrl: "/partials/loadchangesetmenu"};
+    });
 
+
+    simmapeditorApp.controller("StreetMenuController", ['$scope', function($scope) {
+        $scope.menuState="rootMenu";
+    }]);
     simmapeditorApp.controller("StreetDetailController", ['$scope', function($scope) {
-        $scope.streetModel = null
+        $scope.streetModel = null;
         $scope.layer = null;
         $scope.marker = null;
         $scope.newFeature = function (feature, layer, latlng, map) {
-            var streetDetails = $("#streetDetails")
+            var streetDetails = $("#streetDetails");
             $scope.streetModel = feature;
 
             $scope.streetModel.properties.freespeedCalculated = parseInt($scope.streetModel.properties.freespeed * 3.6);
@@ -22,20 +32,14 @@
             $scope.$apply();
         };
         $scope.removeFeature = function (map, beforeNew) {
-            var streetDetails = $("#streetDetails")
+            var streetDetails = $("#streetDetails");
             if (!beforeNew) streetDetails.offcanvas("hide");
             $scope.streetModel = null;
-            var layer = $scope.layer;
-            if (layer != null) {
-                // Set color?
-            }
-            ;
             $scope.layer = null;
             var marker = $scope.marker;
             if (marker != null) {
                 map.removeLayer(marker);
             }
-            ;
             $scope.marker = null;
             $scope.$apply();
         };
